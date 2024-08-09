d6cf1ab3a6b1bb67c51c34b5686881baccd10a0e
mxrsel <marsel14082006@gmail.com>
2024-08-09 12:38:16 +0600

    added mui to main and created theme.ts for mui to styling text fields

--- cipher-frontend/src/main.tsx
+++ cipher-frontend/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import {ThemeProvider} from "@mui/material";
+import theme from "./theme.ts";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+      <ThemeProvider theme={theme}>
     <App />
+      </ThemeProvider>
   </StrictMode>,
 )
