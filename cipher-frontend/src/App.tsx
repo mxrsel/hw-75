b0af1ac57b8f03980f4df9669472e8410c4309c6
mxrsel <marsel14082006@gmail.com>
2024-08-09 13:51:20 +0600

    created function for encoding text on page and added to text field

--- cipher-frontend/src/App.tsx
+++ cipher-frontend/src/App.tsx
@@ -1,23 +1,35 @@
 import React, {useState} from 'react';
 import { TextField, Button, Container, Stack } from '@mui/material';
+import axiosApi from "./axiosApi.ts";
 
 const App: React.FC = () => {
     const [message, setMessage] = useState('');
     const [password, setPassword] = useState('');
     const [result, setResult] = useState('');
 
+
+    const handleEncode = async () => {
+        try {
+            const response = await axiosApi.post('/encode', { message, password });
+            setResult(response.data.encoded);
+        } catch (error) {
+            console.error('Error encoding text:', error);
+        }
+    };
     return (
         <Container>
             <Stack spacing={2}>
                 <TextField
                     label="Message"
                     variant="outlined"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
                 />
                 <TextField
                     label="Password"
                     variant="outlined"
                 />
-                <Button variant="contained">Encode</Button>
+                <Button variant="contained" onClick={handleEncode}>Encode</Button>
                 <Button variant="contained" >Decode</Button>
                 <TextField
                     label="Result"
